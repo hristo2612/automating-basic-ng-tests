863c610edf0d2ec4905cdc943f4716e0c8898cfd
hristo2612 <hristoapps@gmail.com>
2018-11-26 23:13:50 +0200

    Update main file and add variables.js file - holding our projectPath and our components paths array - will be used to construct a basic test for each one in the array

--- auto.js
+++ auto.js
@@ -1,45 +1,21 @@
 const path = require('path');
 const read = require('./utils/readFile');
 const write = require('./utils/writeFile');
+const projectPath = require('./variables').projectPath;
+const componentPaths = require('./variables').componentPaths;
 
-let contents = read.readFile(path.resolve('C://Users/Hristo/Github/angular-fundamentals/src/app/product-list', 'product-list.component.ts'));
+const currentFilePath = path.resolve(projectPath, componentPaths[1]);
 
-// Get constructor dependencies
+let contents = read.readFile(currentFilePath);
+//let getType = read.getFileType(path.resolve())
 
-let dependencies = contents.match(/constructor\((.+?)(\D+?)(\))/g);
+let dependencies = read.getConstructorDependencies(contents);
+console.log(dependencies);
 
-// remove whitespaces, tabs and newlines
-dependencies = dependencies[0].replace(/(\s{2,})/g, '')
-    .replace('constructor(', '')
-    .replace(')', '');
-dependencies = dependencies.split(',')
-// Get instances names
-let instances = dependencies.map((dep) => {
-    return dep.trim()
-        .split(':')[0]
-        .split(' ')[1];
-});
+let importStatements = read.getImportStatements(contents, dependencies.dependencyNames);
+console.log(importStatements);
 
-// And type names
-let types = dependencies.map((dep) => {
-    return dep.trim()
-        .split(':')[1]
-        .split(' ')[1];
-});
+let usedMethods = read.getInstanceMethods(contents, dependencies.instanceNames);
+console.log(usedMethods);
 
-
-console.log('Our dependencies', types);
-
-instances.forEach(element => {
-    let regex = new RegExp("([^\\/])(this\\.)" + element + "(\\.{1})(\\w+)", "g");
-    let matches = contents.match(regex);
-    if (matches) {
-        matches = matches.map((match) => {
-            return match.split('.')[2];
-        });
-
-        matches = [...new Set(matches)];
-
-        console.log("Methods for " + element + ": ", matches);
-    }
-});
+write.writeFile('fuck.txt', importStatements[0]);
---
+++ variables.js
@@ -0,0 +1,8 @@
+module.exports = {
+    projectPath: "C:/Users/hristob/projects/[SOMEPROJECT]/src/app",
+    componentPaths: [
+        'somecomponent',
+        'somepipe',
+        'someService'
+    ]
+}
